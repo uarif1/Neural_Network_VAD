24a69a0cdcdb06830af2ff83ad972e00bc27ba7a
Usama Arif <usamaarif26@gmail.com>
2018-06-12 16:45:40 +0100

    corrected put from tofrq funcs, many indexing issues and msks

--- voicebox/melbankm.py
+++ voicebox/melbankm.py
@@ -73,10 +73,9 @@ def melbankm(p, n, fs, fl=0, fh=0.5):
 
     # Note "FFT bin_0" assumes DC = bin 0 whereas "FFT bin_1" means DC = bin 1
 
-    sfact = 2
     mflh = np.array([fl, fh])*fs
 
-    mflh = frq2mel(mflh)  # convert frequency limits into mel
+    mflh, _ = frq2mel(mflh)  # convert frequency limits into mel
 
     melrng = mflh[1] - mflh[0]  # mel range
     # bin index of highest positive frequency(Nyquist if n is even)
@@ -90,24 +89,24 @@ def melbankm(p, n, fs, fl=0, fh=0.5):
     # Calculate the FFT bins corresponding to
     # [filter  # 1-low filter#1-mid filter#p-mid filter#p-high]
     #
-    blim = mel2frq(mflh[0]+np.array([0, 1, p, p+1])*melinc)*n/fs
+    blim = mel2frq(mflh[0]+np.array([0, 1, p, p+1])*melinc)[0]*n/fs
 
     mc = mflh[0]+np.arange(1, p+1)*melinc  # mel centre frequencies
     # lowest FFT bin_0 required might be negative)
-    b1 = np.floor(blim[0])+1
-    b4 = min(fn2, np.ceil(blim[3])-1)
+    b1 = int(np.floor(blim[0])+1)
+    b4 = int(min(fn2, np.ceil(blim[3])-1))
     # highest FFT bin_0 required
     #
     # now map all the useful FFT bins_0 to filter1 centres
     #
-    pf = (frq2mel(np.arange(b1, b4+1)*fs/n)-mflh[0])/melinc
+    pf = (frq2mel(np.arange(b1, b4+1)*fs/n)[0]-mflh[0])/melinc
     #
     # remove any incorrect entries in pf due to rounding errors
     #
     if pf[0] < 0:
         pf = pf[1:]
         b1 = b1+1
-    if pf(-1) >= p+1:
+    if pf[-1] >= p+1:
         pf = pf[:-1]
         b4 = b4-1
     # FFT bin_0 i contributes to filters_1 fp(1+i-b1)+[0 1]
@@ -119,28 +118,29 @@ def melbankm(p, n, fs, fl=0, fh=0.5):
     # filters
     k2 = np.where(fp > 0)[0]
     k2 = k2[0] if len(k2) > 0 else k4+1
-    # FFT bin_1 k3+b1 is the last to contribute to both upper and lower filters
+    # FFT bin_1 k3+b1 is the last to contribute to both upper and lower
+    # filters
     k3 = np.where(fp < p)[0]
     k3 = k3[-1] if len(k3) > 0 else 0
 
-    r = np.concatenate((1+fp[:k3], fp[k2-1, k4]))  # filter number_1
+    r = np.concatenate((1+fp[:k3+1], fp[k2: k4])
+                       ).astype(int)  # filter number_1
     # FFT bin_1 - b1
-    c = np.concatenate((np.arange(1, k3+1), np.arange(k2, k4+1)))
-    v = np.concatenate((pm[:k3], 1-pm[k2:k4]))
-    mn = b1+1  # lowest fft bin_1
-    mx = b4+1  # highest fft bin_1
+    c = np.concatenate((np.arange(1, k3+2), np.arange(k2+1, k4+1)))
+    v = np.concatenate((pm[:k3+1], 1-pm[k2:k4+1]))
+    mn = b1  # lowest fft bin_1
+    mx = b4  # highest fft bin_1
     if b1 < 0:
         # convert negative frequencies into positive
         c = np.abs(c+b1-1)-b1+1
 
     # double all except the DC and Nyquist(if any) terms
-    if sfact == 2:
-        # there is no Nyquist term if n is odd
-        msk = (c+mn > 2) & (c+mn < n-fn2+2)
-        v[msk] = 2*v[msk]
+    # there is no Nyquist term if n is odd
+    msk = (c+mn > 1) & (c+mn < n-fn2+1)
+    v[msk] = 2*v[msk]
     #
     # sort out the output argument options
     #
 
-    x = csr_matrix(v, (r, c))
+    x = csr_matrix((v, (r-1, c-1)))
     return x, mc, mn, mx
